ccb466446414196e04840f663d71f3bc7a89395a
Алексей Бобков <bobkovalex.yaundex.ru@yandex.ru>
2024-06-21 18:53:40 +0300

    init

--- Computer_app/output/output_window2.py
+++ Computer_app/output/output_window2.py
@@ -21,48 +21,37 @@ def list_page():
     global entr2
     global lbl3
     if counter == 1:
-        # 0 -> NO
-        # 1 -> YES and if it was placed -> destroy
-        place_or_not = 0
-        # 0 -> NO
-        # 1 -> YES
-        was_it_place_or_not = 0
-        a = entr0.get()
-        if len(a) == 0 and place_or_not == 0:
-            place_or_not = 1
-            was_it_place_or_not = 1
-            lbl3.place(x=500, y=500)
+        counter_1 = 0
+        if len(entr0.get()) == 0 and counter_1 == 0:
+            counter_1 = 1
+            lbl3.grid(row=2, column=0, padx=(480, 10), pady=(50, 0))
         else:
-            if place_or_not == 1 and len(a) > 0 and was_it_place_or_not == 1:
+            if counter_1 == 1:
                 lbl3.destroy()
                 lbl3 = Label(window, text='Заполните все поля', font='Arial 20', bg='red', fg='white')
-                place_or_not = 0
-                print('YES')
-        if place_or_not == 0:
-            list1 = {'login': a}
-            lbl3.destroy()
+        if counter_1 == 0:
+            list1 = [entr0.get()]
             lbl0.destroy()
             entr0.destroy()
             lbl1 = Label(window, text='Введите ваш пароль:', font='Arial 20', bg='green', fg='white')
             entr1 = Entry(window, font='Arial 20', bg='white', fg='black')
-            # lbl1.place(x=, y=)
-            # entr1.place(x=, y=)
+            lbl1.grid(row=0, column=0, padx=(480, 10), pady=(200, 0))
+            entr1.grid(row=0, column=0, padx=(480, 10), pady=(280, 0))
 
             lbl2 = Label(window, text='Повторите ваш пароль:', font='Arial 20', bg='green', fg='white')
             entr2 = Entry(window, font='Arial 20', bg='white', fg='black')
-            # lbl2.place(x=, y=)
-            # entr2.place(x=, y=)
+            lbl2.grid(row=1, column=0, padx=(480, 10), pady=(100, 0))
+            entr2.grid(row=1, column=0, padx=(480, 10), pady=(180, 0))
             counter += 1
-            print(list1)
     if counter == 2:
         entr1 = entr1.get()
         entr2 = entr2.get()
         if entr1 != entr2:
             lbl3 = Label(window, text='Пароли не совпадают', font='Arial 20', bg='red', fg='white')
-            # lbl3.place(x=, y=)
+            lbl3.grid(row=2, column=0, padx=(480, 10), pady=(50, 0))
         elif len(entr1) == 0 or len(entr2) == 0:
             lbl3 = Label(window, text='Заполните все поля', font='Arial 20', bg='red', fg='white')
-            # lbl3.place(x=, y=)
+            lbl3.grid(row=2, column=0, padx=(480, 10), pady=(50, 0))
         else:
             pass
     if counter == 3:
@@ -83,13 +72,13 @@ if len(f) == 0:
     file_settings = open('settings.txt', 'r+', encoding='utf-8')
 
     lbl0 = Label(window, text='Введите ваш логин:', font='Arial 20', bg='green', fg='white')
-    lbl0.place(x=500, y=250)
+    lbl0.grid(row=0, column=0, padx=(480, 10), pady=(300, 0))
     entr0 = Entry(window, font='Arial 20', bg='white', fg='black')
-    entr0.place(x=480, y=300)
+    entr0.grid(row=1, column=0, padx=(480, 10), pady=(10, 0))
     # btn0 = Button(window, text='Отправить', font='Arial 20', command=is_right_registration)
     # btn0.grid(row=2, column=0, padx=480, pady=(300, 0))
     btn0 = Button(window, text='Далее', font='Arial 30', command=list_page)
-    btn0.place(x=1100, y=730)
+    btn0.grid(row=3, column=1)
 
     # lbl1 = Label(window, text='Введите ваш пароль:', font='Arial 20', bg='green', fg='white')
     # lbl1.grid(row=0, column=1)

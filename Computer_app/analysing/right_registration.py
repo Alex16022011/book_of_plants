a377e429c4ddbc8df5dd130f066e982b61d1ebc1
Alex Bobkov <bobkovalex.yaundex.ru@yandex.ru>
2024-06-25 11:33:49 +0300

    init

--- Computer_app/analysing/right_registration.py
+++ Computer_app/analysing/right_registration.py
@@ -22,35 +22,42 @@ def right_email(email):
 
 def right_phone_number(phone):
     # if pass:
-    #     return True
+    return True
     return False
 
 def right_registration(first_name, second_name, third_name, date, email, login, password1, password2, phone):
     global to_change
-    counter = 1
+    counter = 0
+
+    if login.strip() > 0:
+        counter += 1
+    else:
+        to_change += 'ввести логин который состоит не только из пробелов,'
 
     if first_name.isalpha() and second_name.isalpha() and third_name.isalpha():
         counter += 3
+    else:
+        to_change += 'имя, фамилия, отчество должны состоять только из букв,'
 
     if check_on_right_date(date):
         counter += 1
     else:
-        to_change += 'ввести правильную дату формата DD.MM.YYYY (ДД.ММ.ГГГГ)'
+        to_change += 'ввести правильную дату формата DD.MM.YYYY (ДД.ММ.ГГГГ),'
 
     if email:
         counter += 1
     else:
-        to_change += 'ввести правильный email'
+        to_change += 'ввести правильный email,'
 
     if password1 == password2:
         counter += 2
     else:
-        to_change += 'ввести одинаковые пароли'
+        to_change += 'ввести одинаковые пароли,'
 
     if right_phone_number(phone):
         counter += 1
     else:
-        to_change += 'ввести правильный номер телефона'
+        to_change += 'ввести правильный номер телефона.'
 
     if counter == 9:
         if not os.path.exists('Дневник семян.txt'):

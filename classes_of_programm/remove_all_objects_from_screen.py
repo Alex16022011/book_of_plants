20c138ade0db741407cbf05a1fb4e7591b315acb
Alex Bobkov <bobkovalex.yaundex.ru@yandex.ru>
2024-07-21 22:49:25 +0300

    Preparing

---
+++ classes_of_programm/remove_all_objects_from_screen.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('.')
+from customtkinter import *
+ 
+def destroy_all_from_screen(root, method: str) -> None:
+    method = method.lower()
+    if method == 'pack':
+        list1 = root.pack_slaves()
+    elif method == 'grid':
+        list1 = root.grid_slaves()
+    elif method == 'place':
+        list1 = root.slaves()
+    for i in list1:
+        i.destroy()

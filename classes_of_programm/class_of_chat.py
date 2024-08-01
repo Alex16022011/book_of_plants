c9c1446aa9f466b2f852de590a4f94880a61c646
Алексей Бобков <bobkovalex.yaundex.ru@yandex.ru>
2024-08-01 16:59:30 +0300

    chat_create

--- classes_of_programm/class_of_chat.py
+++ classes_of_programm/class_of_chat.py
@@ -1,102 +1,42 @@
 import sys
-
 sys.path.append('.')
 from customtkinter import *
-from Computer_app.about_computer.size_of_screen import know_the_size_of_screen
+from Computer_app.about_computer.size_of_screen import know_the_size_of_screen as sizes
 from classes_of_programm.remove_all_objects_from_screen import destroy_all_from_screen
+from CTkListbox import *
+from threading import *
+from chat.user import user
+
+def send_messages(event):
+    global text
+    message = text.get('0.0', END)
+    user(message)
+    text.delete('0.0', END)
+    
+root = CTk()
+h = sizes()['height'] - 70
+h2 = int(str(h)[:])
+w = sizes()['width'] + 1
+print(w)
+root.geometry(f'{w}x{h}+{-10}+0)')
+root.resizable(0, 0)
+
+listbox = CTkListbox(root)
+listbox.pack(fill="both", expand=True, padx=10, pady=10)
+
+text = CTkTextbox(master=root, font=('Arial', 20), bg_color='black', fg_color='white', height=70, width = (w - 131), border_color='black', border_width=2)
+text.event_add('<<Paste>>', '<Control-igrave>')
+text.event_add("<<Copy>>", "<Control-ntilde>")
+# text.bind("<Control-Return>", command=lambda a: text.insert(END, '\n'))
+text.bind("<Return>", command=lambda event: send_messages(event))
+text.pack(side=LEFT, anchor=CENTER, padx=(10, 10), pady=(0, 10))
+btn_send = CTkButton(master=root, text='➤', fg_color='#3399FF', text_color='white', font=('Arial', 30), height=50, width=20, corner_radius=1000, command=lambda: (send_messages(a := 5), 
+                                                                                                                                                                   text.delete('1.0', END)))
+btn_send.pack(side=RIGHT, anchor=SE, padx=(10, 20), pady=(0, 10))
 
 
-def return_to_previous_page(page: str) -> None:
-    # dct_of_pages = {'main_page': {'menu_of_chat': 'chat', 'dictionary_materials': ['ills', 'bat_moscitos'] }}
-    # if page == 'chat':
-    #     root = MenuOfChats()
-    main_root = MenuOfChats()
-    frm = main_root.create_top_of_menu_chat(name_of_window=root, width=width1 - 2, height=height1 - (height1 - 50))
-    frm2 = main_root.create_main_frame(name_of_window=root, width=width1 - 2, height=height1 - 50, foreground='#33FF33')
-    main_root.place_exit_button(frame=frm)
-    main_root.place_name_of_menu(frame=frm)
-    main_root.place_adder_button()
+listbox.insert(0, "Option 0")
+listbox.insert("END", "Option 8")
 
 
-class MenuOfChats(CTk):
-    def create_new_chat(self):
-        global frm
-        global frm2
-        global btn3
-        btn3.destroy()
-
-        def go_to_menu_of_chats() -> None:
-            destroy_all_from_screen(root, 'pack')
-            btn3 = CTkButton(master=frm2, text='+', corner_radius=1000, bg_color='#33FF33', font=('Arial', 40), width=3,
-                             command=self.create_new_chat)
-            btn3.place(x=width1 - 555, y=height1 - 555)
-
-        def place_exit_button_when_create_a_chat() -> None:
-            global btn3
-            btn = CTkButton(master=root, text='←', font=('Arial', 20), width=100, height=47,
-                            command=go_to_menu_of_chats)
-            btn.pack(anchor='nw')
-
-        place_exit_button_when_create_a_chat()
-        lbl = CTkLabel(master=root, text='Название чата:', font=('Arial', 30), bg_color='#33FF33')
-        lbl.pack(anchor='center')
-        entr1 = CTkEntry(master=root, font=('Arial', 30), width=500)
-        entr1.pack(anchor='center')
-
-        btn2 = CTkButton(master=root, text='Создать чат', font=('Arial', 30), command=return_to_previous_page)
-        btn2.pack(expand=True)
-
-    def create_top_of_menu_chat(self, name_of_window: str, width: int, height: int) -> CTkFrame:
-        frm = CTkFrame(master=name_of_window, border_width=1, height=height, width=width, bg_color='white')
-        frm.place(x=1, y=0)
-        return frm
-
-    def create_main_frame(self, name_of_window: str, width: int, height: int, foreground='white') -> CTkFrame:
-        frm2 = CTkFrame(master=name_of_window, border_width=1, height=height, width=width, fg_color=foreground)
-        frm2.place(x=1, y=height1 - (height1 - 50))
-        return frm2
-
-    def place_exit_button(self, frame) -> None:
-        btn = CTkButton(master=frame, text='←', font=('Arial', 20), width=100, height=47)
-        btn.pack(side=LEFT, padx=(0, 400))
-
-    def place_name_of_menu(self, frame):
-        lbl = CTkLabel(master=frame, text='Чаты садоводов и огородников:', font=('Arial', 30))
-        lbl.pack(side=TOP, padx=(0, height1 - (100 + 400 + lbl.winfo_width())))
-
-    def place_adder_button(self):
-        global btn3
-        btn3 = CTkButton(master=frm2, text='+', corner_radius=1000, bg_color='#33FF33', font=('Arial', 40), width=3,
-                         command=self.create_new_chat)
-        btn3.place(x=width1 - 555, y=height1 - 555)
-
-
-class Chat:
-    def create_top_of_the_chat(self, window, height: int, width: int, foreground: str = 'white') -> None:
-        frm = CTkFrame(master=window, border_width=1, height=height, width=width, fg_color=foreground)
-        frm.place(x=0, y=0)
-
-    def create_part_with_messages(self, window, height: int, width: int, foreground: str = 'white') -> None:
-        frm = CTkFrame(master=window, border_width=1, height=height, width=width, fg_color=foreground)
-        frm.place(x=0, y=0)
-
-    def create_part_with_typing_this_message(self, window, height: int, width: int, foreground: str = 'white') -> None:
-        frm = CTkFrame(master=window, border_width=1, height=height, width=width, fg_color=foreground)
-        frm.place(x=0, y=0)
-
-
-if __name__ == '__main__':
-    root = CTk()
-    root.config(bg='#33FF33')
-    root.title('Дневник садовода и огородника')
-    width1 = know_the_size_of_screen()['width']
-    height1 = know_the_size_of_screen()['height']
-    print(width1, height1)
-    root.geometry(f'{width1}x{height1}+{-10}+{0}')
-    main_root = MenuOfChats()
-    frm = main_root.create_top_of_menu_chat(name_of_window=root, width=width1 - 2, height=height1 - (height1 - 50))
-    frm2 = main_root.create_main_frame(name_of_window=root, width=width1 - 2, height=height1 - 50, foreground='#33FF33')
-    main_root.place_exit_button(frame=frm)
-    main_root.place_name_of_menu(frame=frm)
-    main_root.place_adder_button()
-    root.mainloop()
+root.mainloop()
--- classes_of_programm/class_of_main_page.py
+++ classes_of_programm/class_of_main_page.py
@@ -3,37 +3,5 @@ import sys
 sys.path.append('.')
 from customtkinter import *
 from Computer_app.about_computer.size_of_screen import know_the_size_of_screen
+from classes_of_programm.remove_all_objects_from_screen import destroy_all_from_screen
 
-
-class MainPage():
-    def title_main(self, window, width1: int, height1: int):
-        frm = CTkFrame(master=window, width=width1, height=height1 - (height1 - 50))
-        frm.pack()
-
-        lbl = CTkLabel(master=frm, text='Меню', font=('Arial', 30))
-        center_lbl = width1 // 2
-        lbl.pack(padx=(center_lbl - 235, center_lbl - 295), side=LEFT)
-
-        btn = CTkButton(master=frm, text='\u2699', font=('Arial', 30), width=50)
-        btn.pack(side=RIGHT)
-
-    def btn_of_chat(self, window):
-        btn = CTkButton(master=window, text='Чаты', font=('Arial', 20))
-        btn.pack()
-
-    def btn_of_help(self, window):
-        btn2 = CTkButton(master=window)
-
-
-if __name__ == '__main__':
-    root = CTk()
-    root.config(bg='#33FF33')
-    root.title('Дневник садовода и огородника')
-    width1 = know_the_size_of_screen()['width']
-    height1 = know_the_size_of_screen()['height']
-    print(width1, height1)
-    root.geometry(f'{width1}x{height1}+{-10}+{0}')
-    main_root = MainPage()
-    main_root.title_main(root, width1, height1)
-    main_root.btn_of_chat(root)
-    root.mainloop()
